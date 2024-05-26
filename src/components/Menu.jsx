cde1d90a29e3090370e43ad4ed3100076da992d7
davidvchang <dvalenzuelachang@gmail.com>
2024-05-25 22:23:18 -0700

    Se terminó la parte una parte del menú

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,8 +8,12 @@ function Menu() {
         <Nav icon={iconHome} text='Home'/>
         <Nav icon={iconExplore} text='Explore'/>
         <Nav icon={iconNotifications} text='Notifications'/>
-        <Nav icon={iconHome} text='Home'/>
+        <Nav icon={iconMessage} text='Messages'/>
+        <Nav icon={iconGrok} text='Grok'/>
+        <Nav icon={iconLists} text='Lists'/>
+        <Nav icon={iconBookmarks} text='Bookmarks'/>
         <Nav icon={iconCommunities} text='Communities'/>
+        <Nav icon={iconPremium} text='Premium'/>
         <Nav icon={iconProfile} text='Profile'/>
         <Nav icon={iconMoreNav} text='More'/>
         
@@ -42,6 +46,27 @@ const iconProfile = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox=
 const iconMoreNav = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-6 iconNav">
 <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
 </svg>
+
+const iconMessage = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 iconNav">
+<path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+</svg>
+
+const iconGrok = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-square-slash iconNav">
+  <rect width="18" height="18" x="3" y="3" rx="2"/><line x1="9" x2="15" y1="15" y2="9"/>
+</svg>
+
+const iconLists = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-clipboard-list iconNav">
+  <rect width="8" height="4" x="8" y="2" rx="1" ry="1"/><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"/><path d="M12 11h4"/><path d="M12 16h4"/><path d="M8 11h.01"/><path d="M8 16h.01"/>
+</svg>
+
+const iconBookmarks = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 iconNav">
+<path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+</svg>
+
+const iconPremium = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={26} height={26} color={"#ffffff"} fill={"none"}>
+<path d="M3 21L10.5484 13.4516M21 3L13.4516 10.5484M13.4516 10.5484L8 3H3L10.5484 13.4516M13.4516 10.5484L21 21H16L10.5484 13.4516" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+</svg>
+
 
 
 
